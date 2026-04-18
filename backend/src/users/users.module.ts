0c0849e3176227d8783d2421953f63b32c8f3459
Gyovanna Garcês <gyovannab23@gmail.com>
2026-04-18 18:46:12 -0300

    feat: registra entity User no UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
-// esse módulo expõe o UsersService pra quem precisar usar
-// o AuthModule vai importar ele pra conseguir verificar os usuários no login
+/**
+ * Módulo responsável pelo gerenciamento de usuários.
+ * Registra a entity User no TypeORM e disponibiliza
+ * o UsersService para outros módulos.
+ */
 @Module({
+  imports: [
+    // Registra a entity User para uso do TypeORM neste módulo
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [UsersService],
+  // Exporta o UsersService para que o AuthModule possa utilizá-lo
   exports: [UsersService], // sem isso aqui o AuthModule não enxerga o serviço
 })
 export class UsersModule {}

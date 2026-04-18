e9d818ae882a75b227faecf524b6970b7babb208
Gyovanna Garcês <gyovannab23@gmail.com>
2026-04-18 18:31:16 -0300

    feat: configura TypeORM e ambiente de desenvolvimento local

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports:    [
+    ConfigModule.forRoot({ isGlobal: true }),
+    
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
